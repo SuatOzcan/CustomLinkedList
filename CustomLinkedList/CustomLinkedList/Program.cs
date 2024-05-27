c497c89a13407470f7f3711844ff6fbd20bf74d3
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-05-27 11:09:22 +0300

    Create ILinkedList interface.

---
+++ CustomLinkedList/CustomLinkedList/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+public interface ILinkedList<T> : ICollection<T>
+{
+    void AddToFront(T item);
+    void AddToEnd(T item);
+}
+
+
