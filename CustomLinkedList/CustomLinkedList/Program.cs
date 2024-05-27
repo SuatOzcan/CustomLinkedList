e4d6ed2968d011b196d0d1e5a233cc3684e39112
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-05-27 12:11:09 +0300

    Create SinglyLinkedList class. Implement AddToTheFront method.

--- CustomLinkedList/CustomLinkedList/Program.cs
+++ CustomLinkedList/CustomLinkedList/Program.cs
@@ -1,8 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more 
+
+using System.Collections;
+
+SinglyLinkedList<string> list = new SinglyLinkedList<string>();
+list.AddToFront("a");
+list.AddToFront("b");
+list.AddToFront("c");
+
+Console.ReadKey();
 public interface ILinkedList<T> : ICollection<T>
 {
     void AddToFront(T item);
     void AddToEnd(T item);
+}
+
+
+public class SinglyLinkedList<T> : ILinkedList<T?>
+{
+    private Node<T>? _head;
+    private int _count;
+    public int Count => _count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddToEnd(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddToFront(T? item)
+    {
+        Node<T> newHead = new Node<T>(item) { Next = _head };
+        _head = newHead;
+        ++_count;
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T?[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T? item)
+    {
+        throw new NotImplementedException();
+    }
+    public IEnumerator<T?> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class Node<T>
